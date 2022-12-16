5d2d3f3d0623f998dff106c2010ea60c31114b37
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2022-12-17 00:28:51 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sergei Shekshuev on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
